74a178005890e06f803b0d3d3de0eb4a93c704f1
Oleksandr Chaika <pti4ka121@gmail.com>
2023-03-24 13:05:48 +0200

    make build

--- src/scripts/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const form = document.querySelector('#form');
-
-form.addEventListener('submit', onSubmit, false);
-
-function onSubmit(event) {
-  event.preventDefault();
-  form.reset();
-};
